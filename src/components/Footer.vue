b37670f45fa23378059aa4b562e43a947ee81261
Šarūnas Lekstutis <sarunas.lekstutis@gmail.com>
2022-01-30 15:44:19 +0200

    footer styling completed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,7 @@
         <a href="#">Careers</a>
         <a href="#">Contacts</a>
       </div>
-      <div class="footer-column">
+      <div class="footer-column socials">
         <a href="#"
           ><img src="../assets/images/icon-facebook.svg" alt="facebok"
         /></a>
@@ -42,29 +42,54 @@
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/_variables.scss";
 
 .footer {
-    .footer-wrapper {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        .footer-column {
-            display: flex;
-            flex-direction: column;
-            justify-content: start;
-            a {
-                text-decoration: none;
-            }
-            a img {
-                background-color: red;
-            }
-        }
+  .footer-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: $veryDarkViolet;
+    color: $white;
+    padding: 3rem 4rem;
+    .footer-column {
+      display: flex;
+      flex-direction: column;
+      justify-content: start;
+      gap: 1rem;
+      h3 {
+        margin: 0;
+        font-size: 2rem;
+      }
+      h5 {
+        font-size: 1.1rem;
+        margin: 0;
+        margin-bottom: 1.2rem;
+      }
+      a {
+        font-size: 1.1rem;
+        font-weight: 500;
+        text-decoration: none;
+        color: $grayishViolet;
+      }
+      &:nth-child(1) {
+        justify-content: start;
+        width: 25%;
+      }
+      &:last-child {
+          width: 25%;
+      }
     }
+    .socials {
+      display: flex;
+      flex-direction: row;
+      gap: 2rem;
+      justify-content: end;
+    }
+  }
 }
 </style>

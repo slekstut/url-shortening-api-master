8addd7cbb8465bc04b2f14538ca9e699aebba43e
Šarūnas Lekstutis <sarunas.lekstutis@gmail.com>
2022-02-23 20:59:48 +0200

    started responsive design on mobile menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,18 +2,27 @@
   <nav>
     <div class="nav-left">
       <div class="logo"><img src="../assets/images/logo.svg" alt="" /></div>
-      <ul>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Resources</li>
-      </ul>
+      <div :class="{ 'active-mobile-menu': activeMobileMenu }">
+        <ul>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+      </div>
+      <div class="auth-btns">
+        <LoginBtn
+          :class="{ 'active-btn': isActive === 1 }"
+          @click="isActive = 1"
+          >Login</LoginBtn
+        >
+        <SignupBtn
+          :class="{ 'active-btn': isActive === 2 }"
+          @click="isActive = 2"
+          >Sign Up</SignupBtn
+        >
+      </div>
     </div>
-
-    <div class="auth-btns">
-      <LoginBtn :class="{ 'active-btn': isActive === 1 }" @click="isActive=1">Login</LoginBtn>
-      <SignupBtn :class="{ 'active-btn': isActive === 2 }" @click="isActive=2">Sign Up</SignupBtn>
-    </div>
-    <div class="menu-icon">
+    <div class="menu-icon" @click="activeMobileMenu = !activeMobileMenu">
       <span class="line"></span>
       <span class="line"></span>
       <span class="line"></span>
@@ -29,6 +38,7 @@ export default {
   data() {
     return {
       isActive: 2,
+      activeMobileMenu: false,
     };
   },
 };
@@ -73,23 +83,63 @@ nav {
   .auth-btns {
     display: flex;
     button:first-child {
-        margin-right: .5rem;
+      margin-right: 0.5rem;
     }
   }
   .menu-icon {
-    position: absolute;
-    top: 2rem;
-    right: 2rem;
-    width: 32px;
-    height: 26px;
     display: none;
-    flex-direction: column;
-    justify-content: space-between;
-    .line {
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  nav {
+    width: 90%;
+    .nav-left {
+      ul {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding-top: 2.5rem;
+        padding-bottom: 1rem;
+        background-color: $darkViolet;
+        position: absolute;
+        top: 4rem;
+        left: 50%;
+        transform: translate(-50%);
+        right: 1rem;
+        width: 100%;
+        min-height: 50vh;
+        height: 100%;
+        z-index: 100;
+        border-radius: .7rem;
+          li {
+            color: $white;
+            margin-bottom: 2rem;
+            &:last-child {
+              border-bottom: 1px solid $grayishViolet;
+            }
+          }
+      }
+    }
+    .auth-btns {
       display: none;
-      width: 100%;
-      height: 4px;
-      background: $grayishViolet;
+    }
+    .menu-icon {
+      display: flex;
+      position: absolute;
+      top: 1rem;
+      right: 0.5rem;
+      width: 32px;
+      height: 26px;
+      flex-direction: column;
+      justify-content: space-between;
+      .line {
+        display: block;
+        width: 100%;
+        height: 4px;
+        background: $grayishViolet;
+      }
     }
   }
 }
